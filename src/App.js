d08e0c81d3758ae335b054841fa2e7450649aa0c
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-03-21 18:34:22 +1300

    Todo signin page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage/LandingPage';
+import SignInPage from './components/SignInPage/SignInPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path='/'><LandingPage /></Route>
+          <Route exact path='/'><LandingPage /></Route>
+          <Route path='/signin'><SignInPage /></Route>
         </Switch>
       </Router>
     </div>
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import lighthouse from '../../images/lighthouse.svg';
 import { HiChevronRight, HiArrowNarrowRight } from 'react-icons/hi';
 
@@ -13,20 +12,12 @@ const LandingPage = () => {
 
   return (
     <main className={styles.wrapper}>
-      <Router>
-        <img className={styles.logo} src={lighthouse} alt='A cartoon lighthouse' />
-        <div className={styles.container}>
-          <h1 className={styles.title}>Litehaus</h1>
-          <p className={styles.subtitle}>Keep an eye on your favourite stocks.</p>
-          <Link className={styles.button} to='/signin' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>Sign in{arrow}</Link>
-        </div>
-
-        <Switch>
-          <Route path='/signin'>
-            {/* TODO: add sign in page */}
-          </Route>
-        </Switch>
-      </Router>
+      <img className={styles.logo} src={lighthouse} alt='A cartoon lighthouse' />
+      <div className={styles.container}>
+        <h1 className={styles.title}>Litehaus</h1>
+        <p className={styles.subtitle}>Keep an eye on your favourite stocks.</p>
+        <a className={styles.button} href='/signin' onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>Sign in{arrow}</a>
+      </div>
     </main>
   );
 };
