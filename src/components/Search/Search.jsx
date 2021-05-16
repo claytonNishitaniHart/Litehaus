dca0d495e01897db8c4dc94b3358df4a45031525
claytonNishitaniHart <claytonnishitanihart@gmail.com>
2021-05-16 20:55:25 +1200

    debounce wasnt working with async, switched to AwesomeDebouncePromise

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,31 +1,27 @@
 import React, { useState } from 'react';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 import styles from './Search.module.css';
 
 const Search = ({ updateSymbols, currentSymbols }) => {
   const [results, setResults] = useState(null);
+
   const search = async (input) => {
     const response = await fetch(`https://finnhub.io/api/v1/search?q=${input}&token=${process.env.REACT_APP_FH_TOKEN}`);
     const json = await response.json();
-    input.length === 0 ? setResults(null) : setResults(json.result);
+    return json;
   };
 
-  const debounce = (func, wait) => {
-    let timer;
+  const searchAPIDebounced = AwesomeDebouncePromise(search, 250);
 
-    return function executedFunc(...args) {
-      const later = () => {
-        timer = null;
-        func(...args);
-      }
-      clearTimeout(timer);
-      timer = setTimeout(later, wait);
-    };
-  }
+  const handleChange = async input => {
+    const result = await searchAPIDebounced(input);
+    input.length === 0 ? setResults(null) : setResults(result.result);
+  };
 
   return (
     <form className={styles.form}>
       <label className={styles.label}>Search</label>
-      <input className={`${styles.input} ${results && results.length > 0 ? styles.inputShowing : ''}`} onChange={(e) => debounce(search(e.target.value), 300)} />
+      <input className={`${styles.input} ${results && results.length > 0 ? styles.inputShowing : ''}`} onChange={(e) => handleChange(e.target.value)} />
       <ul className={results ? styles.results : styles.hidden}>
         {results ? results.map((element, index) => {
           return (<li key={index}><p className={styles.symbol}>{element.displaySymbol}</p> <p className={styles.description}>{element.description}</p> <button className={styles.button} onClick={(e) => updateSymbols(e, element.displaySymbol)}>{currentSymbols.split(',').includes(element.displaySymbol) ? '-' : '+'}</button></li>);
